d6abdeaad24f1a47c949624efd7c01610f45b89a
Deepak Gupta <deepak.gupta1@unthinkable.co>
2022-06-18 15:26:19 +0530

    added projects section

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+import newsArcub from '../images/newsArcub.jpeg';
+import foodzen from '../images/foodzen.jpg';
+import zerokkata from '../images/zerokkata.png';
+const products = [
+    {
+      id: 1,
+      img: newsArcub,
+      links: "https://github.com/deepak2310gupta/NewsArcub",
+    },
+    {
+      id: 2,
+      img: foodzen,
+      links: "https://github.com/deepak2310gupta/FoodZen",
+    },
+    {
+      id: 3,
+      img: newsArcub,
+      links: "https://github.com/deepak2310gupta/SafeMed",
+    },
+    {
+      id: 4,
+      img: zerokkata,
+      links: "https://github.com/deepak2310gupta/mywebdesign",
+    },
+  ];
+
+
+  export default products;
